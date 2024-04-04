800af04cb04cef5a9a872bb6cc70a765ad024fd4
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-04-04 11:46:03 -0600

    Delete Sample Content

--- cypress/e2e/smoke.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Smoke Test', () => {
-  it('can viwe the home page', () => {
-    cy.visit('/');
-    cy.contains('Opinion Ate Starter');
-  });
-});
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Opinion Ate Starter</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
