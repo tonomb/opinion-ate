df8d9ca5deca826a6323fac17288447350011860
Antonio Martinez Baez <38362926+tonomb@users.noreply.github.com>
2024-04-04 12:45:09 -0600

    refactor tests

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -2,25 +2,31 @@ import {render, screen} from '@testing-library/react';
 import RestaurantList from '../RestaurantList';
 
 describe('RestaurantList', () => {
-  it('Loads restaurants on first render', () => {
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
+  const restaurants = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
+
+  let loadRestaurants;
+
+  function renderComponent() {
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
 
     render(
-      <RestaurantList loadRestaurants={loadRestaurants} restaurants={[]} />,
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
     );
+  }
 
+  it('Loads restaurants on first render', () => {
+    renderComponent();
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
   it('Should Display the restaurants', () => {
-    const noop = () => {};
-
-    const restaurants = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
-
-    render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
+    renderComponent();
 
     expect(screen.getByText('Sushi Place')).toBeInTheDocument();
     expect(screen.getByText('Pizza Place')).toBeInTheDocument();
